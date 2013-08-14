0455feb8c506adbb2479ad723633ad2bc8405142
Ubuntu <ubuntu@ip-172-31-27-111.ap-southeast-2.compute.internal>
2013-08-14 10:45:01 +0000

    edited mysql query for testing s3332059_partB.php

--- s3332059_partB.php
+++ s3332059_partB.php
@@ -6,17 +6,20 @@
   
 <body>
 
-<?php $result = mysql_query("Select * from region"); ?>
 
-// $result is populated from mysql query
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
   <select value="tableName">
+
   <?php
+  $result = mysql_query("SELECT region_name FROM region");
+
   while($row = mysql_fetch_row($result)) {
   $tableName = $row[0];
   echo '<option value="$tableName">$tableName</option>';
   }
+
   ?>
+
   </select>
   <?php // you will need another drop down list here ?>
   <input type="submit" name="submit" value="Run Query">
