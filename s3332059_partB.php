135aaee062a813a315d6c7e03971c8feba2882f1
Ubuntu <ubuntu@ip-172-31-27-111.ap-southeast-2.compute.internal>
2013-08-14 11:02:28 +0000

    added code to be able to connect to database s3332059_partB.php

--- s3332059_partB.php
+++ s3332059_partB.php
@@ -5,7 +5,25 @@
   </head>
   
 <body>
+<?php
 
+ require_once('db.php');
+
+ if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW))
+        {
+                echo 'Could not connect to mysql on ' . DB_HOST . '\n';
+                exit;
+        }
+
+        if(!mysql_select_db(DB_NAME, $dbconn))
+        {
+     echo 'Could not user database ' . DB_NAME . '\n'; echo mysql_error() . '\n';
+          exit;
+        }
+
+        mysql_select_db('winestore');
+
+        ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
   <select value="tableName">
