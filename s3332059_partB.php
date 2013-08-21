3a6b68ff8fb0b6162931727bec33c9480ebcb78d
Ubuntu <ubuntu@ip-172-31-27-111.ap-southeast-2.compute.internal>
2013-08-21 08:47:29 +0000

    Updated code for grape variety drop down list

--- s3332059_partB.php
+++ s3332059_partB.php
@@ -26,24 +26,38 @@
         ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-  <select value="tableName">
-
+  Region </br>
+     
+  <select value="regionName">
   <?php
   $result = mysql_query("SELECT region_name FROM region");
 
   while($row = mysql_fetch_array($result)) {
-  ?>
- 
-
- <option value="$tableName"><?php echo $row["region_name"]?></option>';
-  
+ ?> 
+  <option value="$regionName"><?php echo $row["region_name"]?></option>;
 <?php
-}
-?>
+ }
  
-
+?>
   </select>
-  <?php // you will need another drop down list here ?>
+
+ Grape Variety </br>
+
+  <select value="grapeVariety">
+  <?php
+  $result = mysql_query("SELECT variety FROM grape_variety");
+
+  while($row = mysql_fetch_array($result)) {
+  ?>
+  
+  <option value="$grapeVariety"><?php echo $row["variety"]?></option>;
+<?php
+ }
+
+?>
+
+</select>
+
   <input type="submit" name="submit" value="Run Query">
 </form>
 
